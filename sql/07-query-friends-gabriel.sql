67157c9512cf0b9d6c83f5660460a41b31f12156
Rai Lee <thisisrailee@gmail.com>
2017-03-24 10:05:04 -0700

    Made changes based on feedback.

--- sql/07-query-friends-gabriel.sql
+++ sql/07-query-friends-gabriel.sql
@@ -13,4 +13,17 @@ SELECT id1 FROM student.friends WHERE id2 IN (SELECT id FROM student.student WHE
 SELECT id2 FROM student.friends WHERE id1 IN (SELECT id FROM student.student WHERE name = 'Gabriel');
 
 /*Picks names from student.student table from given ids from line 10 and 12*/
-SELECT name from student.student WHERE id IN (SELECT id1 FROM student.friends WHERE id2 IN (SELECT id FROM student.student WHERE name = 'Gabriel')) OR id IN (SELECT id2 FROM student.friends WHERE id1 IN (SELECT id FROM student.student WHERE name = 'Gabriel'));
+SELECT
+  name
+from
+  student.student
+WHERE id IN (
+  SELECT
+    id1
+  FROM
+    student.friends
+  WHERE
+    id2 IN (SELECT id FROM student.student WHERE name = 'Gabriel')) OR
+    id IN (SELECT id2 FROM student.friends WHERE id1 IN (
+      SELECT id FROM student.student WHERE name = 'Gabriel')
+    );
